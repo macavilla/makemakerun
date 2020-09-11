2e451f646718e878183aba8365813e937c7ddf5d
maca-quuack <macarena.villa@quuack.com>
2020-09-11 00:52:58 -0300

    isolate Sketch function

---
+++ src/components/Sketch/Sketch.js
@@ -0,0 +1,15 @@
+
+
+let Sketch = (p, width, height) => {
+     p.setup = () => {
+      p.createCanvas(width, height);
+      p.frameRate(30);
+    }
+
+     p.draw = () => {
+      p.background([150, 10, 10]);
+    }
+
+
+  }
+
